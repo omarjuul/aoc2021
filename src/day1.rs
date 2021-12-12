3a5cc51ff0085c9c18d1d88b4a69b92cc00e75b5
Camiel Egbers <camiel.egbers@gmail.com>
2021-12-12 18:04:19 +0100

    cargo fmt

--- src/day1.rs
+++ src/day1.rs
@@ -1,18 +1,11 @@
-﻿#[aoc_generator(day1)]
+#[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<u32> {
-    input
-        .lines()
-        .map(|l| l.parse::<u32>().unwrap())
-        .collect()
+    input.lines().map(|l| l.parse::<u32>().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
 pub fn run(input: &[u32]) -> usize {
-    let increases = input
-        .windows(2)
-        .map(|w| w[0] < w[1])
-        .filter(|b| *b)
-        .count();
+    let increases = input.windows(2).map(|w| w[0] < w[1]).filter(|b| *b).count();
 
     increases
 }
@@ -39,15 +32,15 @@ mod tests {
 
         assert_eq!(result, 1400);
     }
-    
+
     #[test]
-    fn input_known_answer_part2(){
+    fn input_known_answer_part2() {
         let result = run_windowed_average(&input_generator(INPUT));
 
         assert_eq!(result, 1429);
     }
 
-    const EXAMPLE: [u32;10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    const EXAMPLE: [u32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
     #[test]
     fn example_increases_7_times() {

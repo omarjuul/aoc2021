ca86f197ae5b0cea66b413fa76b9b5279fc91386
Camiel Egbers <camiel.egbers@gmail.com>
2022-11-13 21:59:37 +0100

    complete day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -20,14 +20,17 @@ pub struct OctoField {
 
 impl OctoField {
     fn new(entries: Vec<u8>) -> Self {
-        OctoField {entries, flashes: 0}
+        OctoField {
+            entries,
+            flashes: 0,
+        }
     }
-    
-    fn step(&mut self) {
+
+    fn step(&mut self) -> u64 {
         for entry in self.entries.iter_mut() {
             *entry += 1;
         }
-        
+
         let mut prev_len = 101;
         let mut flashed: HashSet<usize> = HashSet::new();
         while flashed.len() != prev_len {
@@ -35,13 +38,15 @@ impl OctoField {
             self.handle_flashes(&mut flashed);
             // println!("{} octo's flashed", flashed.len());
         }
-        self.flashes += flashed.len() as u64;
+        let num_flashed = flashed.len() as u64;
         for idx in flashed {
             self.entries[idx] = 0;
         }
+        self.flashes += num_flashed;
         // self.print();
+        num_flashed
     }
-    
+
     fn handle_flashes(&mut self, flashed: &mut HashSet<usize>) {
         let mut to_up = Vec::new();
         for (index, entry) in self.entries.iter_mut().enumerate() {
@@ -56,20 +61,20 @@ impl OctoField {
             self.up_neighbours(index);
         }
     }
-    
+
     fn up_neighbours(&mut self, index: usize) {
         let neighbours = self.neighbours_of(index);
         for neighbour in neighbours {
             self.entries[neighbour] += 1;
         }
     }
-    
+
     fn neighbours_of(&self, index: usize) -> Vec<usize> {
         let mut result = Vec::new();
         let can_go_left = index % 10 > 0;
         let can_go_right = index % 10 < 9;
         let can_go_up = index > 9;
-        let can_go_down = index < 90; 
+        let can_go_down = index < 90;
         if can_go_up {
             result.push(index - 10);
             if can_go_left {
@@ -94,9 +99,9 @@ impl OctoField {
                 result.push(index + 11)
             }
         }
-        result        
+        result
     }
-    
+
     fn print(&self) {
         for line in self.entries.chunks(10) {
             println!("{:?}", line);
@@ -114,9 +119,17 @@ pub fn run(input: &OctoField) -> u64 {
     input.flashes
 }
 
-// #[aoc(day11, part2)]
-// pub fn run_p2(input: &[u32]) -> u64 {
-// }
+#[aoc(day11, part2)]
+pub fn run_p2(input: &OctoField) -> u64 {
+    let mut input = input.clone(); // ugly but it works
+    for i in 1.. {
+        let num_flashes = input.step();
+        if num_flashes >= 100 {
+            return i;
+        }
+    }
+    unreachable!()
+}
 
 #[cfg(test)]
 mod tests {
@@ -131,12 +144,12 @@ mod tests {
         assert_eq!(result, 1739);
     }
 
-    // #[test]
-    // fn input_known_answer_p2() {
-    //     let result = run_p2(&input_generator(INPUT));
-    //
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn input_known_answer_p2() {
+        let result = run_p2(&input_generator(INPUT));
+
+        assert_eq!(result, 324);
+    }
 
     const EXAMPLE: &str = "5483143223
 2745854711
@@ -154,5 +167,12 @@ mod tests {
         let actual = run(&input_generator(EXAMPLE));
 
         assert_eq!(actual, 1656);
+    }
+
+    #[test]
+    fn example_p2() {
+        let actual = run_p2(&input_generator(EXAMPLE));
+
+        assert_eq!(actual, 195);
     }
 }
